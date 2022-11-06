ecd766b64850b9f65c7fb4187aff60291d8c4454
SirJohn42Walker <111967604+SirJohn42Walker@users.noreply.github.com>
2022-11-06 18:38:17 +0100

    add font, got toggle answer working

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -1,24 +1,30 @@
 import "./Card.css";
 
-export default function Card({ card, onBookmarkToggle }) {
+export default function Card({ card, onBookmarkToggle, onAnswerToggle }) {
   const handleBookmarkToggle = () => {
     onBookmarkToggle(card.id);
+  };
+  const handleAnswerToggle = () => {
+    onAnswerToggle(card.id);
   };
   return (
     <div className="card">
       <img
         src={
           card.bookmarked
-            ? "/Icons/bookmark-pngrepo-com (1).png"
-            : "/Icons/bookmark-pngrepo-com (2).png"
+            ? "/Icons/bookmark-pngrepo-com (3).png"
+            : "/Icons/bookmark-pngrepo-com (4).png"
         }
         alt="click here to bookmark this card"
         className="bookmark"
         onClick={handleBookmarkToggle}
       />
-      <p>{card.question}</p>
-      <button className="Showanswer">Show Answer</button>
-      <p>{card.answer}</p>
+      {<p>{card.question}</p> /*bleibt unberührt*/}
+
+      <button className="Showanswer" onClick={handleAnswerToggle}>
+        Show Answer
+      </button>
+      <p>{card.answervisible ? card.answer : card.answerhidden}</p>
     </div>
   );
 }
